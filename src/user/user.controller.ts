94e86e558b93aa502705c823ed6c0c60e0683122
ymabsout <ymabsoute@gmail.com>
2025-06-01 21:27:08 +0100

    healthCheck verification

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,9 +2,15 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { user_authData } from "../controllers/auth_intra";
 import { userAccountCreation } from "../utils/userCreation.js";
 
-export const healthCheck = (req: FastifyRequest, resp: FastifyReply) => {
-  console.log("valid endpoint hit , backend is UP");
-  resp.status(200).send({ message: "Valid /healthCheck hit" });
+export const healthCheck = async (req: FastifyRequest, resp: FastifyReply) => {
+  try {
+    await req.jwtVerify();
+    const userData: user_authData = await req.jwtDecode();
+    console.log("valid endpoint hit , backend is UP");
+    return resp.status(200).send({ message: "Valid /healthCheck hit" });
+  } catch (err) {
+    return resp.status(404).send({ error: "Not found" });
+  }
 };
 
 export const getUserData = async (
@@ -14,6 +20,7 @@ export const getUserData = async (
   try {
     await req.jwtVerify();
     const userData: user_authData = await req.jwtDecode();
+    // This function returns the Data of the user
     return resp.status(200).send(userData);
   } catch (e) {
     console.log(" ----<> catched Error", e);
