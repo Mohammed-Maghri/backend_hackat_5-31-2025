ea31fa810c715405e8e87d761e999a3cd6c32f8c
ymabsout <ymabsoute@gmail.com>
2025-06-02 11:13:14 +0100

    test commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,14 @@ import { user_authData } from "../controllers/auth_intra";
 import { Orm_db } from "../orm.js";
 
 export const healthCheck = async (req: FastifyRequest, resp: FastifyReply) => {
-  try {
+  try {  
     await req.jwtVerify();
     const userData: user_authData = await req.jwtDecode();
     console.log("valid endpoint hit , backend is UP");
     console.log(req.raw.url, " query params in health check");
-    const {expo_notification_token} = req.query as {expo_notification_token : string};
+    const { expo_notification_token } = req.query as {
+      expo_notification_token: string;
+    };
     console.log(expo_notification_token);
     if (expo_notification_token) {
       console.log("Expo notification token:", expo_notification_token);
@@ -20,7 +22,17 @@ export const healthCheck = async (req: FastifyRequest, resp: FastifyReply) => {
         condition: `where login = '${userData.login}'`,
       });
     }
-    return resp.status(200).send({ message: "Valid /healthCheck hit" });
+    const userCheck = (await Orm_db.selection({
+      server: req.server,
+      table_name: "users",
+      colums_name: ["*"],
+      command_instraction: `where login = '${userData.login}'`,
+    })) as user_authData[];
+    if (userCheck.length === 0) {
+      return resp.status(404).send({ error: "User not found" });
+    }
+    // This function returns the Data of the user
+    return resp.status(200).send(userCheck[0]);
   } catch (err) {
     return resp.status(404).send({ error: "Not found" });
   }
@@ -34,12 +46,12 @@ export const getUserData = async (
     await req.jwtVerify();
     const userData: user_authData = await req.jwtDecode();
     // check if the user is in database
-    const userCheck = await Orm_db.selection({
+    const userCheck = (await Orm_db.selection({
       server: req.server,
       table_name: "users",
       colums_name: ["*"],
       command_instraction: `where login = '${userData.login}'`,
-    }) as user_authData[];
+    })) as user_authData[];
     if (userCheck.length === 0) {
       return resp.status(404).send({ error: "User not found" });
     }
